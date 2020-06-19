c1e44821cb60157c4461227dc357e12d4144b258
dinoxas <dhassiotis07@gmail.com>
2020-06-19 17:44:25 +0100

    created coin grid

--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AppContext } from '../App/AppProvider';
+import { fontSize1, greenBoxShadow, color3 } from '../Shared/Styles';
 
 const ConfirmButtonStyled = styled.button`
-  color: #03ff03;
-  background: black;
+  background: transparent;
+  font-family: 'Do Hyeon', sans-serif;
+  border: 2px solid ${color3};
+  color: ${color3};
+  ${fontSize1}
   cursor: pointer;
   margin: 1rem;
   padding: 0.5rem 1rem;
+
+  &:hover {
+    ${greenBoxShadow}
+  }
 `;
 
 export const CenterDiv = styled.div`
