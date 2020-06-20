222914bb7c1179bcd7cb051de2b9ed54d7df488b
dinoxas <dhassiotis07@gmail.com>
2020-06-20 15:31:00 +0100

    made grid responsive with auto-fill

--- src/App/AppLayout.js
+++ src/App/AppLayout.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  padding: 2.5rem;
+  padding: 2rem 1rem;
+  max-width: 1200px;
+  margin: 0 auto;
+  box-sizing: border-box;
 `;
