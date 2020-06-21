069baaa2bdafa99a8d88d1331706d207018b1fa1
dinoxas <dhassiotis07@gmail.com>
2020-06-21 16:25:00 +0100

    added highcharts theme

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -4,6 +4,9 @@ import { AppContext } from '../App/AppProvider';
 import { Tile } from '../Shared/Tile';
 import ReactHighcharts from 'react-highcharts';
 import highchartsConfig from './HighchartsConfig';
+import highchartsTheme from './HighchartsTheme';
+
+ReactHighcharts.Highcharts.setOptions(highchartsTheme);
 
 export default function () {
   return (
